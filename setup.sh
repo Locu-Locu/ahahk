2ac7e9d14b40eeb351df66d7199276077209fb5f
Locu-Locu <122764123+Locu-Locu@users.noreply.github.com>
2023-01-16 22:15:06 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -47,7 +47,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
-IZIN=$( curl https://raw.githubusercontent.com/muhamadparizan/kebenaran/main/ipdibenarkan | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/Zuz99/lock/main/ip | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 clear
 echo -e "${green}Tahniah! Anda Dibenarkan menggunakan Script Ctech Didik...${NC}"
